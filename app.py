151560b1e7fef116822f75c0569877525a28abb0
lauranonato <laura.mirandamn@gmail.com>
2024-04-22 22:56:53 -0300

    update pagina do streamlit

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+from pipeline_02_usando_duckdb_banco import pipeline
  
-st.write("Testando o front end da aplicacao de dados")
+st.write("Processador de Arquivos:")
+if st.button('Processar'):
+    with st.spinner('Processando...'):
+        logs = pipeline()
+        for log in logs:
+            st.write(log)
